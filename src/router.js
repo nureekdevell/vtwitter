c34ab885ea23a263a0686019fd2ec2e652d766c0
nureekdevell <58046604+nureekdevell@users.noreply.github.com>
2024-02-03 22:25:55 +0500

    added vue-router

---
+++ src/router.js
@@ -0,0 +1,14 @@
+import {createRouter, createWebHistory} from 'vue-router'
+
+const routes = [
+  {
+  path: '/',
+
+  }
+]
+
+
+  export default createRouter({
+    history: createWebHistory(),
+    routes,
+  })
